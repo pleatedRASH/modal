7bfacfe9274611ace1c0bcc457cf71e4395837ea
pleatedRASH <110006508+pleatedRASH@users.noreply.github.com>
2022-09-18 20:27:00 -0400

    Add files via upload

---
+++ site1/index.js
@@ -0,0 +1,5 @@
+const buttons = document.querySelectorAll('.card-btn');
+const outerModal = document.querySelector('.modal-outer')
+const modalInner = document.querySelector('.modal-inner')
+
+console.log(buttons)
